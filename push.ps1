17b49a94088fad5a842c31696dcdfa99cd38c2d5
zhliu33 <a299745702@163.com>
2026-03-02 22:37:45 +0800

    deleted:    push.ps1

--- push.ps1
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit with README and Transformer article"
-git branch -M main
-git remote add origin https://github.com/zhliu33/BlogArticle.git
-git push -u origin main --force
